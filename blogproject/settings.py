2f8b90510183770c98cf73eb55dd9fff8e1e5435
nande-dayo <suzuki.itiro48@gmail.com>
2022-04-29 03:42:09 +0000

    from anywhere

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%at^#x3z+@cry%%$1mbdjbuh*ufxsrf^__umg8mqpww8b)*!h('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ubuntuari.pythonanywhere.com',
+]
 
 
 # Application definition
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogpost',
+
 ]
 
 MIDDLEWARE = [
